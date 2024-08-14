b8c0b8bd6d16ff7ec7f7c83fd65f707d864c16d6
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2024-08-14 11:01:16 +0000

    Add environments config Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCUtPi7rJz4IPMONkMn-VXIyaqTPR6QrsE",
+        authDomain: "refundmaster-b6ab3.firebaseapp.com",
+        projectId: "refundmaster-b6ab3",
+        storageBucket: "refundmaster-b6ab3.appspot.com",
+        messagingSenderId: "328301284713",
+        appId: "1:328301284713:web:5fd38cc86aacf593b841f8"
+    }
+};

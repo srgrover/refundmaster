fbe0534fc80ade0dfe3d1702b6f869bf21112b0b
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2024-08-16 11:01:59 +0000

    ad routes for store list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { StoreListComponent } from './components/store-list/store-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: 'stores',
+      loadComponent: () => import('./components/store-list/store-list.component').then(c => StoreListComponent)
+    },
+    { path: '', redirectTo: '/stores', pathMatch: 'full' },  // Redirecciona la ruta base a 'stores'
+    { path: '**', redirectTo: '/stores' }  // Redirecciona cualquier ruta no encontrada a 'stores'
+  ];

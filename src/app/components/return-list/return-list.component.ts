3b30c28fefc4a0bb1d7da74ce703456e8ce38137
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2024-08-16 11:03:05 +0000

    Create return list component

---
+++ src/app/components/return-list/return-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-return-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './return-list.component.html',
+  styleUrl: './return-list.component.css'
+})
+export class ReturnListComponent {
+
+}

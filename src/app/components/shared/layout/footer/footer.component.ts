6abe6814cee42c27d45b0790a3139666d6331e19
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2024-09-12 15:20:58 +0000

    footer navigation add but not completed

--- src/app/components/shared/layout/footer/footer.component.ts
+++ src/app/components/shared/layout/footer/footer.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
+  constructor(private router: Router) { }
+
+  goToReturnFormNew() {
+    this.router.navigate(['/returns/new']);
+  }
 }

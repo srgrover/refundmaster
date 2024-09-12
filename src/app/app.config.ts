62c495cf683b2f360ca23158d8b09326cf80489e
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2024-09-12 15:18:33 +0000

    add firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirebaseApp, initializeApp, getApps } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
@@ -9,6 +9,13 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideFirebaseApp(() => {
+      if (!getApps().length) {
+        return initializeApp(environment.firebaseConfig);
+      } else {
+        return getApps()[0];
+      }
+    }),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth())

6bbc4fd039e9f17f808be6d3b6115c2ce6b8e893
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2024-09-12 15:22:42 +0000

    we can view stores in list and click to edit

--- src/app/components/store-list/store-list.component.ts
+++ src/app/components/store-list/store-list.component.ts
@@ -1,30 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { StoreService, Store } from '../../services/store.service';
 import { CommonModule } from '@angular/common';
+import { ReturnService } from '../../services/return.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-store-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './store-list.component.html',
   styleUrls: ['./store-list.component.css']
 })
 
 export class StoreListComponent {
   stores: Store[] = [];
+  returnCounts: { [storeId: string]: number } = {};
 
-  constructor(private storeService: StoreService) { }
+  constructor(private storeService: StoreService, private returnService: ReturnService, private router: Router) { }
 
   ngOnInit() {
     this.storeService.getStores().subscribe(data => {
       this.stores = data;
-      console.log(this.stores)
+      console.log(data)
+      this.updateReturnCounts();
     });
   }
 
-  deleteStore(storeId: string) {
-    this.storeService.deleteStore(storeId).then(() => {
-      console.log('Store deleted');
+  updateReturnCounts() {
+    const observables = this.stores.map(store => 
+      this.returnService.getReturnsCountByStoreId(store.id || '').then(count => ({
+        storeId: store.id,
+        count
+      }))
+    );
+
+    Promise.all(observables).then(results => {
+      results.forEach(result => {
+        this.returnCounts[result.storeId] = result.count;
+      });
+    }).catch(error => {
+      console.error('Error fetching return counts:', error);
     });
+  }
+
+  goToStoreForm() {
+    this.router.navigate(['/stores/new']);
   }
 }

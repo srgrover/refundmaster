2c928b0e05adb56a0c4c9b84705f973ef33b2d91
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2024-09-12 15:24:31 +0000

    Routing for all components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,48 @@
 import { Routes } from '@angular/router';
 import { StoreListComponent } from './components/store-list/store-list.component';
+import { StoreFormComponent } from './components/store-form/store-form.component';
+import { ReturnListComponent } from './components/return-list/return-list.component';
+import { ReturnFormComponent } from './components/return-form/return-form.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 
 export const routes: Routes = [
     {
       path: 'stores',
       loadComponent: () => import('./components/store-list/store-list.component').then(c => StoreListComponent)
     },
-    { path: '', redirectTo: '/stores', pathMatch: 'full' },  // Redirecciona la ruta base a 'stores'
-    { path: '**', redirectTo: '/stores' }  // Redirecciona cualquier ruta no encontrada a 'stores'
+    {
+      path: 'stores/new',
+      loadComponent: () => import('./components/store-form/store-form.component').then(c => StoreFormComponent)
+    },
+    {
+      path: 'stores/:id/edit',
+      loadComponent: () => import('./components/store-form/store-form.component').then(c => StoreFormComponent)
+    },
+    {
+      path: 'returns',
+      loadComponent: () => import('./components/return-list/return-list.component').then(c => ReturnListComponent)
+    },
+    {
+      path: 'returns/new',
+      loadComponent: () => import('./components/return-form/return-form.component').then(c => ReturnFormComponent)
+    },
+    {
+      path: 'returns/:id/edit',
+      loadComponent: () => import('./components/return-form/return-form.component').then(c => ReturnFormComponent)
+    },
+    {
+      path: 'products',
+      loadComponent: () => import('./components/product-list/product-list.component').then(c => ProductListComponent)
+    },
+    {
+      path: 'products/new',
+      loadComponent: () => import('./components/product-form/product-form.component').then(c => ProductFormComponent)
+    },
+    {
+      path: 'products/:id/edit',
+      loadComponent: () => import('./components/product-form/product-form.component').then(c => ProductFormComponent)
+    },
+    { path: '', redirectTo: '/returns', pathMatch: 'full' },
+    { path: '**', redirectTo: '/returns' }
   ];
